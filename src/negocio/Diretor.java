98ef242530d9c657ff000c240fffc909135b2337
Samuel Belo <sam-uka-54@hotmail.com>
2019-04-29 08:34:44 -0300

    Update Diretor.java

--- src/negocio/Diretor.java
+++ src/negocio/Diretor.java
@@ -1,5 +1,32 @@
 package negocio;
 
-public class Diretor {
+public class Diretor extends Funcionario {
+	private float vlrAdicional;
 
+	
+	@Override
+	public void exibir() {
+		System.out.println(this.toString());
+	}
+
+	@Override
+	public String toString() {
+		return String.format(
+				"%s: %s\n" +
+						"Adicional: %.2f\n",
+						"M".equals(this.getSexo()) ? "Diretor" : "Diretora",
+								super.toString(),
+								this.getVlrAdicional()
+				);
+
+
+	}
+	public float getVlrAdicional() {
+		return vlrAdicional;
+	}
+
+	public void setVlrAdicional(float vlrAdicional) {
+		this.vlrAdicional = vlrAdicional;
+	}
 }
+
